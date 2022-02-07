2ac666e0bcfaed692a3113a9c61fef2b7eb7542d
ammar <mresh.dev@gmail.com>
2022-02-07 22:16:12 +0100

    injecting the card information through a service

---
+++ src/app/main/cards/card.service.ts
@@ -0,0 +1,25 @@
+export class CardService{
+ getCardsInfo(){
+  return[
+    {
+      title: "Card 1",
+      image:"../../../assets/images/cardImage1.jpg",
+      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero autem in tempore, aliquam porro quis officia iste ea aut minus?",
+      cardUrl:"https://www.towa-digital.com/"
+    },
+    {
+      title: "Card 2",
+      image:"../../../assets/images/cardImage2.jpg",
+      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero autem in tempore, aliquam porro quis officia iste ea aut minus?",
+      cardUrl:"https://www.towa-digital.com/"
+    },
+    {
+      title: "Card 3",
+      image:"../../../assets/images/cardImage3.jpg",
+      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero autem in tempore, aliquam porro quis officia iste ea aut minus?",
+      cardUrl:"https://www.towa-digital.com/"
+    }
+  ]
+
+ }
+}
--- src/app/main/cards/cards.component.ts
+++ src/app/main/cards/cards.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CardService } from './card.service';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
+  providers: [CardService]
 })
 export class CardsComponent implements OnInit {
-
+  cards;
   
-  constructor() { }
+  constructor(cardService: CardService) {
+    this.cards = cardService.getCardsInfo();
+   }
 
   ngOnInit(): void {
   }
